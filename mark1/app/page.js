bdb97ad4ce6b7355a670c1030c469428ff4643ba
Amrendra <leostereo1108@gmail.com>
2024-12-17 22:52:24 +0530

    sher added!

--- mark1/app/page.js
+++ mark1/app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
     <div className="flex items-center justify-center min-h-screen flex-col">
       <div className=" items-center text-3xl mb-3 flex flex-col">
         <h2>:Home pages:</h2>
-        <h2>to be build soon x`😉</h2>
+        <h2>to be build soon x`🦁</h2>
       </div>
 
       <div className="flex flex-col items-center bg-blue-800 p-4 text-lg font-semibold rounded-lg text-slate-100 m-8">
