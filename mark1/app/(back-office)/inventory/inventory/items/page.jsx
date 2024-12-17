7dd36d134b9425ac81a0f60091550661291e38a8
Amrendra <leostereo1108@gmail.com>
2024-12-17 22:55:23 +0530

    Update page.jsx

--- mark1/app/(back-office)/inventory/inventory/items/page.jsx
+++ mark1/app/(back-office)/inventory/inventory/items/page.jsx
@@ -3,18 +3,13 @@ import { Shirt } from "lucide-react";
 import Link from "next/link";
 import React from "react";
 
-function Items({ cardOptions}) {
-
+function Items() {
   // const {title, description, icon, link, linkTitle, enabled} = cardOptions;
 
   return (
     <div>
       <FixedHeader />
-      <div className="grid grid-col-1 lg:grid-cols-2 px-16 py-6 gap-x-8 gap-y-8">
-        {
-          cardOptions.map
-        }
-      </div>
+      <div className="grid grid-col-1 lg:grid-cols-2 px-16 py-6 gap-x-8 gap-y-8"></div>
     </div>
   );
 }
