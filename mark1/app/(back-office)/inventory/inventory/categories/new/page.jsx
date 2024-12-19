6aca45850868370d032972c5c08d55f7295a81d4
Amrendra <leostereo1108@gmail.com>
2024-12-19 17:39:18 +0530

    category page made!

--- mark1/app/(back-office)/inventory/inventory/categories/new/page.jsx
+++ mark1/app/(back-office)/inventory/inventory/categories/new/page.jsx
@@ -1,52 +1,46 @@
+"use client";
 import FormHeader from "@/components/dashboard/FormHeader";
-import { ChevronDown, X } from "lucide-react";
+import { ChevronDown, Plus, X } from "lucide-react";
+import { useForm } from "react-hook-form";
+
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
+import TextInput from "@/components/FormInput/TextInput";
+import SubmitButton from "@/components/FormInput/SubmitButton";
 
 function NewCategories() {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
+
+  const [loading, setLoading] = useState(false);
+
+  function onSubmit(data) {
+    console.log(data);
+    reset();
+  }
+
   return (
     <div>
       {/* Header */}
       <FormHeader title="New Category" href="#" />
       {/* Form */}
       <form
-        action=""
+        onSubmit={handleSubmit(onSubmit)}
         className="w-full max-w-4xl p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700 mx-auto my-6"
       >
         <div className="grid gap-4 sm:grid-cols-2 sm:gap-6">
-          <div className="sm:col-span-2">
-            <label
-              htmlFor="name"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            >
-              Product Name
-            </label>
-            <input
-              type="text"
-              name="name"
-              id="name"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-              placeholder="Type product name"
-              required=""
-            />
-          </div>
-          <div className="w-full">
-            <label
-              htmlFor="brand"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            >
-              Brand
-            </label>
-            <input
-              type="text"
-              name="brand"
-              id="brand"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-              placeholder="Product brand"
-              required=""
-            />
-          </div>
+          <TextInput
+            label="Category Title"
+            name="title"
+            register={register}
+            errors={errors}
+          />
         </div>
+        <SubmitButton isLoading={loading} title="Category" />
       </form>
       {/* Footer */}
     </div>
